dbf66b9e2bd8fdf285a4ecfcd3e205ef681057fb
George Moon <george@moon.org.nz>
2020-02-01 15:15:06 +0100

    Updates

---
+++ precache-manifest.1cf2283bd149c218c19e5c5a71410a42.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66c33c7d2ef8fb94c2d84b7dc66570a0",
+    "url": "/nzbbtef-prototype/index.html"
+  },
+  {
+    "revision": "5721b402df2e1238c42e",
+    "url": "/nzbbtef-prototype/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "d18e09ddb01640f1fc43",
+    "url": "/nzbbtef-prototype/static/js/2.9b5d305b.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/nzbbtef-prototype/static/js/2.9b5d305b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5721b402df2e1238c42e",
+    "url": "/nzbbtef-prototype/static/js/main.c4798121.chunk.js"
+  },
+  {
+    "revision": "6593db7928a73e88af75",
+    "url": "/nzbbtef-prototype/static/js/runtime-main.3f3054d6.js"
+  }
+]);
