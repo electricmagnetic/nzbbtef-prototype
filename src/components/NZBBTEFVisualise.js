d0f64831ba6ef61484dd9adcaa6baccb4308a198
George Moon <george@moon.org.nz>
2020-02-02 13:10:02 +0100

    Renaming steps

--- src/components/NZBBTEFVisualise.js
+++ src/components/NZBBTEFVisualise.js
@@ -21,13 +21,13 @@ class NZBBTEFVisualise extends Component {
   }
 
   render() {
-    const two = colourTransform(this.state.nzbbtef);
-    const three = tokenise(two);
+    const one = colourTransform(this.state.nzbbtef);
+    const two = tokenise(one);
 
     return (
       <div className="nzbbtef">
         <section className="my-5">
-          <h2>1. Input</h2>
+          <h2>Input</h2>
           <p>Enter a band combo in NZBBTEF format.</p>
           <InputForm
             onFieldChange={this.handleNZBBTEFChange}
@@ -35,17 +35,17 @@ class NZBBTEFVisualise extends Component {
           />
         </section>
         <section className="my-5">
-          <h2>2. Colour Transformation</h2>
+          <h2>1. Colour Transformation</h2>
           <p>
             The first step is to convert long colour names into short colour names.<br />
-            This is so that colours with two words (e.g. Dark Green) can be appropriately processed.
+            This is so that colours with one words (e.g. Dark Green) can be appropriately processed.
           </p>
           <div className="row">
             <div className="col-md-8 mb-3">
               <div className="card">
                 <div className="card-body">
-                  {two
-                    ? <samp>{two}</samp>
+                  {one
+                    ? <samp>{one}</samp>
                     : <span>No input specified</span>
                   }
                 </div>
@@ -53,7 +53,7 @@ class NZBBTEFVisualise extends Component {
             </div>
             <div className="col-md-4 small">
               <ol>
-                <li>Colours can be written as one or two words (Pale Blue or PaleBlue)</li>
+                <li>Colours can be written as one or one words (Pale Blue or PaleBlue)</li>
                 <li>Colour names are case insensitive</li>
                 <li>Colour names will not feature elsewhere (e.g. as symbols or as inscriptions)</li>
               </ol>
@@ -61,7 +61,7 @@ class NZBBTEFVisualise extends Component {
           </div>
         </section>
         <section className="my-5">
-          <h2>3. Tokenisation</h2>
+          <h2>2. Tokenisation</h2>
           <p>
             The second step is to recognise the constituent parts in order to figure out the
             structure of the band combo.
@@ -70,28 +70,27 @@ class NZBBTEFVisualise extends Component {
             <div className="col-md-8 mb-3">
               <div className="card">
                 <div className="card-body">
-                  {three && three.length > 0
-                    ? <TokenTable tokens={ three } />
+                  {two && two.length > 0
+                    ? <TokenTable tokens={ two } />
                     : <span>No input specified</span>
                   }
                 </div>
               </div>
             </div>
-            <div className="col-md-4">
+            <div className="col-md-4 small">
 
             </div>
           </div>
         </section>
         <section className="my-5">
-          <h2>4. Colour Validation</h2>
-          <em>TODO</em>
+          <h2>3. Get Colours</h2>
         </section>
         <section className="my-5">
-          <h2>5. Separator Validation</h2>
-          <em>TODO</em>
+          <h2>4. Validation</h2>
+          <em>TODO: validate colours, validate band structure</em>
         </section>
         <section className="my-5">
-          <h2>6. Structure Creation</h2>
+          <h2>5. Structure Creation</h2>
           <em>TODO</em>
         </section>
       </div>
